ea8e1cdd10cf34b03036c63a3189d7c2a235e2eb
HanHangit <clonkcool@yahoo.de>
2016-08-16 15:20:41 +0200

    Initialize

---
+++ StarDeMarket/Lager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarDeMarket
+{
+
+    enum EItem
+    {
+        Getreide,
+        Holz,
+        Kohle,
+        Count
+    }
+
+    class Lager
+    {
+
+        int[] system = new int[(int)EItem.Count];
+
+        public void Get(EItem item, int value)
+        {
+            system[(int)item] -= value;
+        }
+         
+        public bool Check(EItem item, int value)
+        {
+            if (system[(int)item] >= value)
+                return true;
+            else
+                return false;
+        }
+
+        public void Add(EItem item)
+        {
+            system[(int)item] += 1;
+        }
+
+        public void Add(EItem item, int count)
+        {
+            system[(int)item] += count;
+        }
+
+        public string ToString()
+        {
+            string str = "";
+
+            for (int i = 0; i < system.Length; ++i)
+                str += system[i] + "\n";
+
+            return str;
+        }
+
+    }
+}
---
+++ StarDeMarket/Tile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarDeMarket
+{
+
+    enum ETile
+    {
+        Land,
+        Baum,
+        Erz,
+        Stein,
+        Wasser
+    }
+
+    class Tile
+    {
+
+        public Lager lager;
+
+
+
+        public Tile(ETile type)
+        {
+            lager = new Lager();
+
+            switch (type)
+            {
+                case ETile.Wasser:
+                    lager.Add(EItem.Holz, 1000);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+    }
+}
