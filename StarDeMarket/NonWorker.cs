ca50bd85ca2fef4abdbe02e4682d521fb32e5807
wookiepeter <matthis.hagen@o2online.de>
2016-09-05 17:19:10 +0200

    added in baseclasses for Humans

---
+++ StarDeMarket/NonWorker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace StarDeMarket
+{
+    class NonWorker : Human
+    {
+        
+        public NonWorker(Vector2 _position, EGender _gender)
+        {
+            position = _position;
+            gender = _gender;
+            speed = 2f;
+        }
+
+        public void Update(GameTime gTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StarDeMarket/Woodcutter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace StarDeMarket
+{
+    public class Woodcutter : Worker
+    {
+        public Woodcutter(Vector2 _position, EGender _gender)
+        {
+            position = _position;
+            gender = _gender;
+            speed = 2f;
+        }
+
+        public override void Update(GameTime gTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
