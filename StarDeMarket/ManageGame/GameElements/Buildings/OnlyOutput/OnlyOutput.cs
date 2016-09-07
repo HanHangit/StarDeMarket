dd7f65754149e0b0971c67bdf7cc2d7b276c5612
12Cy <christian.sandkaemper@gmail.com>
2016-09-07 13:01:17 +0200

    push buildings

---
+++ StarDeMarket/ManageGame/GameElements/Buildings/OnlyOutput/OnlyOutput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarDeMarket.ManageGame.GameElements.Buildings.OnlyOutput
+{
+    class OnlyOutput
+    {
+    }
+}
